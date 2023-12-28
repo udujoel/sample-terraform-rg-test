ade57e6aa9569c14fcb6f83f173d870994f1b137
JOEL UDU <joel.udu@ext.mercedes-benz.io>
2023-12-28 15:33:47 -0700

    re-arrangment, add test sample in tfvars

---
+++ provider.tf
@@ -0,0 +1,21 @@
+# Azure Provider source and version
+terraform {
+    backend "local" {
+    path = "state.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+}

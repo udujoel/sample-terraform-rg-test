5de13e9710717b304dc37fd72c34ff5c556d5f27
JOEL UDU <joel.udu@ext.mercedes-benz.io>
2023-12-28 15:29:39 -0700

    updated output

--- modules/resource_groups/outputs.tf
+++ modules/resource_groups/outputs.tf
@@ -1,14 +1,11 @@
-output "resource_group_name" {
-  value       = azurerm_resource_group.resource_groups.name
-  description = "Resource group name"
-}
-
-output "resource_group_id" {
-  value       = azurerm_resource_group.resource_groups.id
-  description = "Resource group generated id"
-}
-
-output "resource_group_location" {
-  value       = azurerm_resource_group.resource_groups.location
-  description = "Resource group location (region)"
+output "resource_groups" {
+  value = {
+    for rg, info in azurerm_resource_group.resource_groups : rg => {
+      id       = info.id
+      name     = info.name
+      location = info.location
+      tags     = info.tags
+    }
+  }
+  description = "output created resource groups with their attributes"
 }

ee149b7e80fd348e9e1ab7088fe8cf558e543f98
JOEL UDU <joel.udu@ext.mercedes-benz.io>
2023-12-28 13:26:37 -0700

    add readme and variables

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "subscription_id" {
+    type = string
+    description = "environment's subscription id"
+    default = ""
+}
+
+variable "client_id" {
+    type = string
+    description = "service principal's id"
+    default = ""
+}
+
+variable "client_secret" {
+    type = string
+    description = "service principal's secret value"
+    default = ""
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Tenant ID for the directory."
+  default = ""
+}
+
+variable "tags" {
+  description = "A map of tags to apply to resources."
+  type        = "map"
+  default     = {}
+}
